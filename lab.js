381d8a38d217ce028210f50412ff0c7de9e3dc2b
U64436 <michael.tihon@bnpparibasfortis.com>
2018-03-13 15:01:57 +0100

    Add promise example

--- lab.js
+++ lab.js
@@ -115,6 +115,23 @@ var wait1000 =  new Promise((resolve, reject) => {
     setTimeout(resolve, 1000)
 }).then(() => console.log('Yay!'));
 
+// *******
+
+const asyncFunc = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          const result = Math.random();
+          result > 0.5 ? resolve(result) : reject('Oppps....I cannot calculate')
+        }, 1)
+    });
+}
+
+for (let i=0; i<10; i++) {
+	asyncFunc()
+    	.then(result => console.log('Result is: ' + result))
+    	.catch(result => console.log('Error: ' + result))
+}
+
 // ---------------------------------------------
 // Classes + getter & setter
 // ---------------------------------------------
